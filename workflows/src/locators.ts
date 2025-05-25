1a903d5ec30d23cebe39981f074373bdd495b5c4
helloujjwal <sujjwal989@gmail.com>
2025-05-25 14:22:21 +0530

    update locator to close popUp modal

--- workflows/src/locators.ts
+++ workflows/src/locators.ts
@@ -1,5 +1,6 @@
 const homePageLocators = {
-    makeMyTripLogo: "div[data-cy='landingContainer'] [data-cy='mmtLogo']"
+    closePopupModal: "div[class='imageSlideContainer'] span[data-cy='closeModal']",
+    makeMyTripLogo: "div[data-cy='landingContainer'] [data-cy='mmtLogo'] img"
 
 
 }
